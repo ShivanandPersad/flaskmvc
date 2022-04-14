df85469117c8aa96aadac46a07582310ee45c7cc
ShivanandPersad <77852460+ShivanandPersad@users.noreply.github.com>
2022-04-14 07:33:18 -0800

    Update config.py

--- App/config.py
+++ App/config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = "sqlite:///temp-database.db"
+SQLALCHEMY_DATABASE_URI = "postgresql://thsbtmgvngfibe:98538b7b051984b41c8148e44d2fd2ecc7b9b8a05a722e1488cf9dc80224ef02@ec2-34-194-158-176.compute-1.amazonaws.com:5432/d7cl0nn2qbbl5r"
 SECRET_KEY = "secret key"
 JWT_EXPIRATION_DELTA = 7
 ENV = "DEVELOPMENT"
